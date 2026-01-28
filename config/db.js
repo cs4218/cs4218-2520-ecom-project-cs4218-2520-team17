bc31f9a46115b262ae91b8b30121acc701a009ec
jiakai-17 <111171022+jiakai-17@users.noreply.github.com>
2026-01-28 11:03:29 +0800

    fix: specify dns server for node and mongoose

--- config/db.js
+++ config/db.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 import colors from "colors";
+
+// Fixes querySrv ECONNREFUSED error by setting custom DNS servers
+// Source - https://stackoverflow.com/a/79870831
+// Posted by Anderson Goulart
+// Retrieved 2026-01-28, License - CC BY-SA 4.0
+import dns from 'node:dns';
+
+console.log('Default DNS servers:', dns.getServers());
+dns.setServers(['1.1.1.1', '1.0.0.1', '8.8.8.8', '8.8.4.4']);
+console.log('Updated DNS servers:', dns.getServers());
+
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL);
