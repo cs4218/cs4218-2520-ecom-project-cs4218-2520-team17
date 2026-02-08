4eebfea9c37344f006618f0ceb81454814cc0528
Sebastian Tay <taysebastian95@gmail.com>
2026-02-08 16:46:07 +0800

    Update enums for order status

--- models/orderModel.js
+++ models/orderModel.js
@@ -16,7 +16,7 @@ const orderSchema = new mongoose.Schema(
     status: {
       type: String,
       default: "Not Process",
-      enum: ["Not Process", "Processing", "Shipped", "Delivered", "Cancel"],
+      enum: ["Not Process", "Processing", "Shipped", "Delivered", "Cancelled"],
     },
   },
   { timestamps: true }
