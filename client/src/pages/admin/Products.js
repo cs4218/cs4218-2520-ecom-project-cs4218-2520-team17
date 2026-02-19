34f835231f5f9faca2f07813ddd8b94c23681998
jiakai-17 <111171022+jiakai-17@users.noreply.github.com>
2026-02-19 16:47:23 +0800

    test: add tests for admin products
    fix: move fetch function into effect

--- client/src/pages/admin/Products.js
+++ client/src/pages/admin/Products.js
@@ -1,43 +1,44 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import AdminMenu from "../../components/AdminMenu";
 import Layout from "./../../components/Layout";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
+
 const Products = () => {
   const [products, setProducts] = useState([]);
 
-  //getall products
-  const getAllProducts = async () => {
-    try {
-      const { data } = await axios.get("/api/v1/product/get-product");
-      setProducts(data.products);
-    } catch (error) {
-      console.log(error);
-      toast.error("Someething Went Wrong");
-    }
-  };
-
-  //lifecycle method
+  // lifecycle method
   useEffect(() => {
+    const getAllProducts = async () => {
+      try {
+        const { data } = await axios.get("/api/v1/product/get-product");
+        setProducts(data.products);
+      } catch (error) {
+        console.error(error);
+        toast.error("Something Went Wrong");
+      }
+    };
+
     getAllProducts();
   }, []);
+
   return (
     <Layout>
-      <div className="row">
-        <div className="col-md-3">
+      <div className="row" data-testid="admin-products-container">
+        <div className="col-md-3" data-testid="admin-products-menu-col">
           <AdminMenu />
         </div>
-        <div className="col-md-9 ">
+        <div className="col-md-9" data-testid="admin-products-main-col">
           <h1 className="text-center">All Products List</h1>
-          <div className="d-flex">
+          <div className="d-flex" data-testid="admin-products-list-container">
             {products?.map((p) => (
               <Link
                 key={p._id}
                 to={`/dashboard/admin/product/${p.slug}`}
                 className="product-link"
               >
-                <div className="card m-2" style={{ width: "18rem" }}>
+                <div className="card m-2" style={{ width: "18rem" }} data-testid="admin-product-card">
                   <img
                     src={`/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
